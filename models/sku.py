e6f0b45b33a8f3bbfd5879c1f2e6da3026eef1fd
mbr <mbrtoc@gmail.com>
2019-04-26 19:55:37 -0400

    [ADD] New functionalities to work orders

--- models/sku.py
+++ models/sku.py
@@ -4,8 +4,8 @@ from odoo import models, fields, api
 class Sku(models.Model):
     _name = 'otif100.sku'
 
-    sku_id = fields.Char(
-        string="SKU",
+    name = fields.Char(
+        string="Product SKU",
         help="Your SKU code"
     )
     fam_id = fields.Many2one(
@@ -18,4 +18,9 @@ class Sku(models.Model):
     buffer = fields.Integer(
         related="fam_id.buffer",
         string="Time buffer",
+    )
+    wo_sku = fields.One2many(
+        comodel_name="otif100.work_order",
+        inverse_name="sku_id",
+        string="Product SKU",
     )
--- models/work_order.py
+++ models/work_order.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import timedelta
 
 
 class Work_order(models.Model):
@@ -14,8 +15,10 @@ class Work_order(models.Model):
     sku_id = fields.Many2one(
         comodel_name="otif100.sku",
         string='Product SKU',
-        delegate=True,
         required=True,
+        ondelete="cascade",
+        help="Your product code",
+
     )
     qty_total = fields.Float(
         string="Total qty",
@@ -27,7 +30,7 @@ class Work_order(models.Model):
     )
     due_date = fields.Date(
         string="Due Date",
-        default=fields.Date.today,
+        default=None,
     )
     actual_release_date = fields.Date(
         string="Actual Release Date",
@@ -37,3 +40,35 @@ class Work_order(models.Model):
         string="Time buffer",
         related="sku_id.buffer",
     )
+    recommended_release_date = fields.Date(
+        compute="_get_recommended_release_date",
+        string="Recommended Release Date",
+    )
+    should_be_released = fields.Boolean(
+        compute="_should_release",
+        string="Should be released",
+    )
+    buffer_penetration = fields.Float(
+        compute="_get_buffer_penetration"
+    )
+
+    @api.depends("due_date", "buffer")
+    def _get_recommended_release_date(self):
+        for r in self:
+            Buffer = timedelta(r.buffer)
+            r.recommended_release_date = r.due_date - Buffer
+
+    @api.depends("recommended_release_date")
+    def _should_release(self):
+        for r in self:
+            if fields.Date.today() < r.recommended_release_date:
+                r.should_be_released = False
+            else:
+                r.should_be_released = True
+
+    @api.depends("recommended_release_date", "buffer")
+    def _get_buffer_penetration(self):
+        for r in self:
+            Buffer_comsumption = (fields.Date.today() -
+                                  r.recommended_release_date).days
+            r.buffer_penetration = 100 * Buffer_comsumption / r.buffer
