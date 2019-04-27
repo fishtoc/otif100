b46dde79b3e3caa85c4fe1872db24c8030b2d3f6
mbr <mbrtoc@gmail.com>
2019-04-27 16:58:40 -0400

    [UPD] Extend functionalities: computes rec release dates, whether order is released, and what action is recommended according to status and dates

--- models/work_order.py
+++ models/work_order.py
@@ -40,17 +40,50 @@ class Work_order(models.Model):
         string="Time buffer",
         related="sku_id.buffer",
     )
+    order_progress = fields.Float(
+        compute="_get_order_progress",
+        string="Order progress",
+    )
     recommended_release_date = fields.Date(
         compute="_get_recommended_release_date",
         string="Recommended Release Date",
+        store=True,
     )
     should_be_released = fields.Boolean(
         compute="_should_release",
         string="Should be released",
+        store=True,
     )
     buffer_penetration = fields.Float(
-        compute="_get_buffer_penetration"
+        compute="_get_buffer_penetration",
+        store=True,
     )
+    buffer_status = fields.Char(
+        string="Buffer color",
+        compute="_get_buffer_status",
+        store=True,
+    )
+    is_released = fields.Boolean(
+        compute="_check_released",
+        string="Already released",
+        store=True,
+    )
+    action_to_take = fields.Char(
+        compute="_get_action",
+        string="DO THIS",
+        help="Obey this instruction and you will get 100% On Time In Full... Believe it or not!!",
+    )
+    need_action = fields.Boolean(
+        compute="_check_action",
+        default=False,
+        string="NEED ATTENTION",
+        store=True,
+    )
+
+    @api.depends("qty_total", "qty_pending")
+    def _get_order_progress(self):
+        for r in self:
+            r.order_progress = 100 * (1 - r.qty_pending / r.qty_total)
 
     @api.depends("due_date", "buffer")
     def _get_recommended_release_date(self):
@@ -72,3 +105,42 @@ class Work_order(models.Model):
             Buffer_comsumption = (fields.Date.today() -
                                   r.recommended_release_date).days
             r.buffer_penetration = 100 * Buffer_comsumption / r.buffer
+
+    @api.depends("buffer_penetration")
+    def _get_buffer_status(self):
+        for r in self:
+            if r.buffer_penetration < 0:
+                r.buffer_status = 'cyan'
+            elif r.buffer_penetration < 33.01:
+                r.buffer_status = 'green'
+            elif r.buffer_penetration < 66.01:
+                r.buffer_status = 'yellow'
+            elif r.buffer_penetration < 100.01:
+                r.buffer_status = 'red'
+            else:
+                r.buffer_status = 'black'
+
+    @api.depends("actual_release_date")
+    def _check_released(self):
+        for r in self:
+            r.is_released = r.actual_release_date
+
+    @api.depends("should_be_released", "is_released")
+    def _get_action(self):
+        for r in self:
+            if r.is_released and not r.should_be_released:
+                r.action_to_take = 'FREEZE THIS ORDER NOW'
+            elif not r.is_released and r.should_be_released:
+                r.action_to_take = 'RELEASE THIS ORDER ASAP'
+            elif not r.is_released and not r.should_be_released:
+                r.action_to_take = 'DO NOT PROCESS YET'
+            else:
+                r.action_to_take = 'PROCESS ACCORDING TO ITS COLOR'
+
+    @api.depends("action_to_take")
+    def _check_action(self):
+        for r in self:
+            if r.action_to_take != 'PROCESS ACCORDING TO ITS COLOR' and r.action_to_take != 'DO NOT PROCESS YET':
+                r.need_action = True
+            else:
+                r.need_action = False
